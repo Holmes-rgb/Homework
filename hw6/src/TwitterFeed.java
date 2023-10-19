c699ccff2b892414f3985ff61978804aa51edbcb
lukeholmes <luke.a.holmes.lh@gmail.com>
2023-10-19 14:51:50 -0400

    Homeworks 10/19

--- hw6/src/TwitterFeed.java
+++ hw6/src/TwitterFeed.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class TwitterFeed {
     private String owner;
-    private ArrayList<String> Tweets = new ArrayList<>(INITIAL_CAPACITY);
+    private ArrayList<String> Tweets;
     private static int INITIAL_CAPACITY = 100;
 
     public TwitterFeed(){
-
+        Tweets = new ArrayList<>(INITIAL_CAPACITY);
     }
 
 }
