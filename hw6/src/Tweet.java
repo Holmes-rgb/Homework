3d0c9572d261c1fb5d3f8d8bcd68cb9a6bdcd1b5
lukeholmes <luke.a.holmes.lh@gmail.com>
2023-10-20 13:49:15 -0400

    Final submitted work

--- hw6/src/Tweet.java
+++ hw6/src/Tweet.java
@@ -3,6 +3,11 @@ public class Tweet {
     private String content;
     public static final int MAX_CHARS = 140;
 
+    /**
+     * Constructor for tweets
+     * @param screenName name of user who created the tweet
+     * @param content content of the tweet as a string
+     */
     public Tweet(String screenName, String content){
         this.screenName = screenName;
         if (content.length() > MAX_CHARS){
@@ -14,32 +19,61 @@ public class Tweet {
 
     }
 
+    /**
+     * Copy constructor for creating a copy of a tweet object
+     * @param other tweet that you would like to copy
+     */
     public Tweet(Tweet other){
         other.screenName = this.screenName;
         other.content = this.content;
     }
 
+    /**
+     * Method to retrieve the content of a tweet
+     * @return content of a tweet as a string
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Method to retrieve the name of the user who created the tweet
+     * @return name of user who created the tweet
+     */
     public String getScreenName() {
         return screenName;
     }
 
+    /**
+     * Set the content of an already existing tweet
+     * @param content string that you would like content of the tweet to be replaced with
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * Method to change the user who created a certain tweet
+     * @param screenName name of user to be used in replacement of the already existing username
+     */
     public void setScreenName(String screenName) {
         this.screenName = screenName;
     }
 
+    /**
+     * Method to print a tweet
+     * @return tweet formatted as string
+     */
     @Override
     public String toString() {
         return String.format("@%s\n\"%s\"",screenName, content);
     }
 
+    /**
+     * Method for comparing two tweets
+     * @param obj Tweet that you would like to compare to this tweet
+     * @return true if they are the same false if they are not
+     */
     @Override
     public boolean equals(Object obj) {
         Tweet other = (Tweet) obj;
--- hw6/src/TwitterFeed.java
+++ hw6/src/TwitterFeed.java
@@ -2,44 +2,72 @@ import java.util.ArrayList;
 
 public class TwitterFeed {
     private String screenName;
-    private ArrayList<Tweet> Tweets;
+    private ArrayList<Tweet> tweets;
     private static int INITIAL_CAPACITY = 100;
 
+    /**
+     * Constructor for Twitter Feed object
+     * @param screenName name of the person whose feed is being constructed
+     */
     public TwitterFeed(String screenName){
-        Tweets = new ArrayList<>(INITIAL_CAPACITY);
+        tweets = new ArrayList<>(INITIAL_CAPACITY);
         this.screenName = screenName;
     }
 
-    //TODO: A getter for the feed that makes a deep copy of the ArrayList and each Tweet reference
-
-    public ArrayList<Tweet> getTweets() {
-        return Tweets;
+    /**
+     * Getter for the Twitter feed
+     * @return Returns a hard copy of the feed arraylist
+     */
+    public ArrayList<Tweet> getTwitterFeed(){
+        ArrayList<Tweet> tweetsCopy = new ArrayList<>();
+        for (int i = 0; i < tweets.size(); i++){
+            tweetsCopy.add(tweets.get(i));
+        }
+        return tweetsCopy;
     }
 
+    /**
+     * Method for adding tweets to the feed
+     * @param screenName name of the user who created the tweet
+     * @param content String with the content of the tweet
+     */
     public void addTweet(String screenName, String content){
-        Tweets.add(new Tweet(screenName, content));
+        tweets.add(new Tweet(screenName, content));
     }
+
+    /**
+     * Method for adding a copy of another tweet to the feed. Essentially a retweet.
+     * @param tweet tweet object that should be copied to feed
+     */
     public void addTweet(Tweet tweet){
-        Tweets.add(tweet);
+        tweets.add(tweet);
     }
 
-
+    /**
+     * Method for printing the Twitter feed
+     * @return the Twitter feed formatted as a string
+     */
     @Override
     public String toString() {
         String feed = "";
         feed += "Feed of @" + screenName + "\n";
-        for (Tweet tweet : Tweets) {
+        for (Tweet tweet : tweets) {
             feed += tweet.toString() + "\n";
         }
         return feed;
     }
 
+    /**
+     * Method for looking up tweets by a user
+     * @param screenName2 Name of user that you would like to see tweets from.
+     * @return Returns all tweets by the user in an easy-to-read format
+     */
     public String tweetsBy(String screenName2){
-        ArrayList<Tweet> tweetsBy = new ArrayList<>(Tweets.size());
+        ArrayList<Tweet> tweetsBy = new ArrayList<>(tweets.size());
 
-        for (int i = 0; i < Tweets.size(); i++){
-            if (Tweets.get(i).getScreenName().equals(screenName2)){
-                tweetsBy.add(Tweets.get(i));
+        for (int i = 0; i < tweets.size(); i++){
+            if (tweets.get(i).getScreenName().equals(screenName2)){
+                tweetsBy.add(tweets.get(i));
             }
         }
 
